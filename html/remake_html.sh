31224b6d08284131ac7f5dda8af8d5c1422f0729
Samuel Webb <samuel.webb@cern.ch>
2020-12-04 16:24:00 +0000

    easier way to make changes to all html at once

---
+++ html/remake_html.sh
@@ -0,0 +1,81 @@
+cp -p index_MTR.html index_SR_2017_MTR.html
+cp -p index_MTR.html index_QCDCR_2017_MTR.html
+cp -p index_MTR.html index_Zee_2017_MTR.html
+cp -p index_MTR.html index_Zmumu_2017_MTR.html
+cp -p index_MTR.html index_Wenu_2017_MTR.html
+cp -p index_MTR.html index_Wmunu_2017_MTR.html
+cp -p index_MTR.html index_SR_2018_MTR.html
+cp -p index_MTR.html index_QCDCR_2018_MTR.html
+cp -p index_MTR.html index_Zee_2018_MTR.html
+cp -p index_MTR.html index_Zmumu_2018_MTR.html
+cp -p index_MTR.html index_Wenu_2018_MTR.html
+cp -p index_MTR.html index_Wmunu_2018_MTR.html
+
+cp -p index_VTR.html index_SR_2017_VTR.html
+cp -p index_VTR.html index_QCDCR_2017_VTR.html
+cp -p index_VTR.html index_Zee_2017_VTR.html
+cp -p index_VTR.html index_Zmumu_2017_VTR.html
+cp -p index_VTR.html index_Wenu_2017_VTR.html
+cp -p index_VTR.html index_Wmunu_2017_VTR.html
+cp -p index_VTR.html index_SR_2018_VTR.html
+cp -p index_VTR.html index_QCDCR_2018_VTR.html
+cp -p index_VTR.html index_Zee_2018_VTR.html
+cp -p index_VTR.html index_Zmumu_2018_VTR.html
+cp -p index_VTR.html index_Wenu_2018_VTR.html
+cp -p index_VTR.html index_Wmunu_2018_VTR.html
+
+sed -i 's/YEAR/2017/g' index_SR_2017_MTR.html
+sed -i 's/YEAR/2017/g' index_QCDCR_2017_MTR.html
+sed -i 's/YEAR/2017/g' index_Zee_2017_MTR.html
+sed -i 's/YEAR/2017/g' index_Zmumu_2017_MTR.html
+sed -i 's/YEAR/2017/g' index_Wenu_2017_MTR.html
+sed -i 's/YEAR/2017/g' index_Wmunu_2017_MTR.html
+sed -i 's/YEAR/2018/g' index_SR_2018_MTR.html
+sed -i 's/YEAR/2018/g' index_QCDCR_2018_MTR.html
+sed -i 's/YEAR/2018/g' index_Zee_2018_MTR.html
+sed -i 's/YEAR/2018/g' index_Zmumu_2018_MTR.html
+sed -i 's/YEAR/2018/g' index_Wenu_2018_MTR.html
+sed -i 's/YEAR/2018/g' index_Wmunu_2018_MTR.html
+sed -i 's/YEAR/2017/g' index_SR_2017_VTR.html
+sed -i 's/YEAR/2017/g' index_QCDCR_2017_VTR.html
+sed -i 's/YEAR/2017/g' index_Zee_2017_VTR.html
+sed -i 's/YEAR/2017/g' index_Zmumu_2017_VTR.html
+sed -i 's/YEAR/2017/g' index_Wenu_2017_VTR.html
+sed -i 's/YEAR/2017/g' index_Wmunu_2017_VTR.html
+sed -i 's/YEAR/2018/g' index_SR_2018_VTR.html
+sed -i 's/YEAR/2018/g' index_QCDCR_2018_VTR.html
+sed -i 's/YEAR/2018/g' index_Zee_2018_VTR.html
+sed -i 's/YEAR/2018/g' index_Zmumu_2018_VTR.html
+sed -i 's/YEAR/2018/g' index_Wenu_2018_VTR.html
+sed -i 's/YEAR/2018/g' index_Wmunu_2018_VTR.html
+
+
+sed -i 's/REGION/SR/g' index_SR_2017_MTR.html
+sed -i 's/REGION/SR/g' index_SR_2018_MTR.html
+sed -i 's/REGION/SR/g' index_SR_2017_VTR.html
+sed -i 's/REGION/SR/g' index_SR_2018_VTR.html
+
+sed -i 's/REGION/QCD CR/g' index_QCDCR_2017_MTR.html
+sed -i 's/REGION/QCD CR/g' index_QCDCR_2018_MTR.html
+sed -i 's/REGION/QCD CR/g' index_QCDCR_2017_VTR.html
+sed -i 's/REGION/QCD CR/g' index_QCDCR_2018_VTR.html
+
+sed -i 's/REGION/Zee/g' index_Zee_2017_MTR.html
+sed -i 's/REGION/Zee/g' index_Zee_2018_MTR.html
+sed -i 's/REGION/Zee/g' index_Zee_2017_VTR.html
+sed -i 's/REGION/Zee/g' index_Zee_2018_VTR.html
+
+sed -i 's/REGION/Zmumu/g' index_Zmumu_2017_MTR.html
+sed -i 's/REGION/Zmumu/g' index_Zmumu_2018_MTR.html
+sed -i 's/REGION/Zmumu/g' index_Zmumu_2017_VTR.html
+sed -i 's/REGION/Zmumu/g' index_Zmumu_2018_VTR.html
+
+sed -i 's/REGION/Wenu/g' index_Wenu_2017_MTR.html
+sed -i 's/REGION/Wenu/g' index_Wenu_2018_MTR.html
+sed -i 's/REGION/Wenu/g' index_Wenu_2017_VTR.html
+sed -i 's/REGION/Wenu/g' index_Wenu_2018_VTR.html
+
+sed -i 's/REGION/Wmunu/g' index_Wmunu_2017_MTR.html
+sed -i 's/REGION/Wmunu/g' index_Wmunu_2018_MTR.html
+sed -i 's/REGION/Wmunu/g' index_Wmunu_2017_VTR.html
+sed -i 's/REGION/Wmunu/g' index_Wmunu_2018_VTR.html
